0900e367176e818400f9f6c5a4ad9016022a45e6
porta62400 <porta62400@students.apsb.org>
2026-08-28 18:07:27 +0000

    platformer update

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -33,26 +33,32 @@ $(function () {
     // TODO 2 - Create Platforms
 createBadPlatform(0, 735, 1400, 50, "red");
 createPlatform(0, 730, 140, 20, "green");
-createPlatform(370, 620, 50, 5, "green")
-createPlatform(630, 550, 100, 5, "green")
-createPlatform(900, 700, 100, 5, "green")
-createPlatform(1000,600, 5, 105, "green")
-createPlatform(1000, 600, 100, 5, "green")
-createPlatform(1200, 500, 100, 5, "green")
-createPlatform(1000, 400, 100, 5, "green")
-createPlatform(200, 500, 100, 5, "green")
-createPlatform(200, 0, 5, 500, "green")
-createPlatform(400, 300, 500, 5, "green")
-createPlatform(400, 180, 500, 5, "green")
-createPlatform(1000, 100, 1400, 5, "green")
+createPlatform(370, 620, 50, 5, "green");
+createPlatform(630, 550, 100, 5, "green");
+createPlatform(900, 700, 100, 5, "green");
+createPlatform(1000,600, 5, 105, "green");
+createPlatform(1000, 600, 100, 5, "green");
+createPlatform(1200, 500, 100, 5, "green");
+createPlatform(1000, 400, 100, 5, "green");
+createPlatform(200, 500, 100, 5, "green");
+createPlatform(200, 0, 5, 500, "green");
+createPlatform(300, 300, 650, 5, "green");
+createPlatform(400, 170, 500, 5, "green");
+createPlatform(1000, 170, 1400, 5, "green");
+createBadPlatform(480, 299, 25, 6, "red");
+createBadPlatform(600, 299, 25, 6, "red");
+createBadPlatform(720, 299, 25, 6, "red");
+createBadPlatform(840, 299, 25, 6, "red");
+createPlatform(1000, 170, 5, 135, "green", 1000, 1000, 0, 35, 170, 1);
     // TODO 3 - Create Collectables
-createCollectable("database", 930, 650)
-createCollectable("max",230, 450)
-
+createCollectable("database", 930, 650);
+createCollectable("max",230, 450);
+createCollectable("steve", 1330, 120);
     
     // TODO 4 - Create Cannons
-
-
+createCannon("right", 630, 1050);
+createCannon("left", 60, 1200);
+createCannon("right", 500, 30000);
     
     
     //////////////////////////////////
